ec470c3bbbae49ecbc5b5f303adbe408507a3c51
Tejas Manu S <64979878+tejas-manu@users.noreply.github.com>
2025-09-26 00:26:43 -0400

    Added CICD for frontend

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -12,3 +12,18 @@ output "sns_topic_arn" {
   description = "The ARN of the SNS topic for notifications."
   value       = aws_sns_topic.notifications.arn
 }
+
+output "frontend_s3_bucket_name" {
+  description = "The name of the S3 bucket for the React frontend."
+  value       = aws_s3_bucket.frontend_bucket.id
+}
+
+output "cloudfront_distribution_id" {
+  description = "The ID of the CloudFront distribution for the frontend."
+  value       = aws_cloudfront_distribution.frontend_distribution.id
+}
+
+output "api_gateway_invoke_url" {
+  description = "The invocation URL for the API Gateway endpoint."
+  value       = aws_apigatewayv2_stage.default_stage.invoke_url
+}
